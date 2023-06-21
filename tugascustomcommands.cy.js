4fff0d445867fe94456dd1bd3160cdc8c3b90948
AzkiRH <azkirafsanjani25@gmail.com>
2023-06-21 14:37:37 +0700

    git commit -m "belajarcypress"

--- tugascustomcommands.cy.js
+++ tugascustomcommands.cy.js
@@ -10,22 +10,15 @@ describe('Working with inputs', () => {
         cy.fixture('user').then(user => {
             const username = user.user
             const password = user.password
-            cy.visit('http://zero.webappsecurity.com/login.html')
             cy.login(username, password)
+            cy.url().should('include', 'account-summary.html')
         })
+    })
     
-    it('Should display online banking content', () => {
-            cy.contains('onlineBankingMenu').click()
-            cy.url().should('include', 'online-banking.html')
-            cy.get('h1').should('be.visible')
-            cy.get('.row').should('be.visible')
-        })
-
     it('Should display pay bills content', () => {
-            cy.contains('pay_bills_link').click()
+            cy.contains('bank/redirect.html?url=pay-bills.html').click()
             cy.url().should('include', 'bank/pay-bills.html')
             cy.get('h2').should('be.visible')
             cy.get('.row').should('be.visible')
         })
     })
-})
